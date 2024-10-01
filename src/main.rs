7c46e37d607c2ee29df74f2de81d50218fdec9b6
MSALEK Mouad <mouadmsalek@outlook.com>
2024-10-01 15:30:42 +0100

    Function written to deduplicate items

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+
+fn get_unique_items<T: PartialEq>(items: Vec<T>) -> Vec<T> {
+    let mut tmp: Vec<T> = Vec::new();
+    for item in items {
+        if !tmp.contains(&item) {
+            tmp.push(item);
+        }
+    }
+    tmp
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    //strings
+    let vec1 = vec!["Hi", "Hi", "Bye", "Greetings", "Apple", "Banana","Bye"];
+    assert_eq!(get_unique_items(vec1), vec!["Hi", "Bye", "Greetings", "Apple", "Banana"]);
+    
+    // i32
+    let vec2 = vec![10, 100, 37, 37, 110, 23, 23, 23, 1];
+    assert_eq!(get_unique_items(vec2), vec![10, 100, 37, 110, 23, 1]);
+    println!("Hoooray all tests passed");
 }
